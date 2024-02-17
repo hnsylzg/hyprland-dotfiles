e1e9fcacea010898fe9c1d35e021b8ba44d6f9f3
hnsylzg <hnsylzg@gmail.com>
2024-02-17 13:40:49 +0800

    Rename playerctl.sh watch_playerctl.sh,Update hyprland.conf

--- .config/hypr/scripts/playerctl.sh
+++
@@ -1,5 +0,0 @@
-[ -x "$(command -v playerctl)" ] && pkill playerctl; 
-playerctl -a metadata --format "{{status}} {{title}}" --follow | while read line; 
-do waybar-signal playerctl; 
-	waybar-signal idle; 
-done
